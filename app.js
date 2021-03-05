8eda30bacb6060c5f02a53a2f99cd130f8006842
sparsh229 <sparsh.gupta229@gmail.com>
2021-03-05 13:18:52 +0530

    added post

--- app.js
+++ app.js
@@ -76,9 +76,10 @@ app.post("/compose", function(req, res){
   
 
 });
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if(port==null||port==""){
+    port = 3000;
+}
+app.listen(port, function (req, res) {
+  console.log("Server is running at port 3000");
 });
